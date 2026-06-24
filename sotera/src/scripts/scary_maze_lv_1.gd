5fc32ef24f398786f59d6405b0cd748baab4b73d
GreyTheFoxx <ararcupcake@gmail.com>
2026-06-23 23:18:16 -0500

    Update scary_maze_lv_1
    - made wall collision smaller
    - added detectable area for walls
    - added placeholder damage taken
    - added placeholder exit

--- sotera/src/scripts/scary_maze_lv_1.gd
+++ sotera/src/scripts/scary_maze_lv_1.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
+	# Player takes damage from walls
+	print("player health -1")
+
+
+func _on_exit_area_body_entered(body: CharacterBody2D) -> void:
+	# Exit to level 2 scene
 	pass
